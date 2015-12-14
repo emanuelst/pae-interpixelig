71b12d82992d7d1b16a16e75e84626fbe9acf03c
emanuelstadler <emanuel.stadler@gmail.com>
2015-12-14 19:38:32 +0100

    remove old fetched results controller (persistent storage) code

--- youtube-app/MasterViewController.swift
+++ youtube-app/MasterViewController.swift
@@ -240,28 +240,6 @@ class MasterViewController: UICollectionViewController, NSFetchedResultsControll
         // Dispose of any resources that can be recreated.
     }
     
-    /*
-    func insertNewObject(sender: AnyObject, videoId: String, title: String) {
-    let context = self.fetchedResultsController.managedObjectContext
-    let entity = self.fetchedResultsController.fetchRequest.entity!
-    let newManagedObject = NSEntityDescription.insertNewObjectForEntityForName(entity.name!, inManagedObjectContext: context)
-    
-    // If appropriate, configure the new managed object.
-    // Normally you should use accessor methods, but using KVC here avoids the need to add a custom class to the template.
-    newManagedObject.setValue(videoId, forKey: "videoId")
-    newManagedObject.setValue(title, forKey: "title")
-    
-    // Save the context.
-    do {
-    try context.save()
-    } catch {
-    // Replace this implementation with code to handle the error appropriately.
-    // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-    //print("Unresolved error \(error), \(error.userInfo)")
-    abort()
-    }
-    }*/
-    
     // MARK: - Segues
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -318,7 +296,6 @@ class MasterViewController: UICollectionViewController, NSFetchedResultsControll
     
     
     override func scrollViewDidScroll(scrollView: UIScrollView) {
-        
         for view in collectionView!.visibleCells(){
             let view:VideoCell = view as! VideoCell
             let yOffset:CGFloat = ((collectionView!.contentOffset.y - view.frame.origin.y) / 200) * 25
@@ -328,7 +305,6 @@ class MasterViewController: UICollectionViewController, NSFetchedResultsControll
     
     
     func configureCell(cell: VideoCell, atIndexPath indexPath: NSIndexPath) {
-        
         if(dict == nil && dict!.count == 0){
             cell.label.text = "xxx"
             
@@ -411,89 +387,6 @@ class MasterViewController: UICollectionViewController, NSFetchedResultsControll
         effectView.frame = self.collectionView!.bounds
 
     }
-    
-    // MARK: - Fetched results controller
-    
-    /*
-    var fetchedResultsController: NSFetchedResultsController {
-        if _fetchedResultsController != nil {
-            return _fetchedResultsController!
-        }
-        
-        let fetchRequest = NSFetchRequest()
-        // Edit the entity name as appropriate.
-        let entity = NSEntityDescription.entityForName("Video", inManagedObjectContext: self.managedObjectContext!)
-        fetchRequest.entity = entity
-        
-        // Set the batch size to a suitable number.
-        fetchRequest.fetchBatchSize = 20
-        
-        // Edit the sort key as appropriate.
-        let sortDescriptor = NSSortDescriptor(key: "videoId", ascending: false)
-        
-        fetchRequest.sortDescriptors = [sortDescriptor]
-        
-        // Edit the section name key path and cache name if appropriate.
-        // nil for section name key path means "no sections".
-        let aFetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.managedObjectContext!, sectionNameKeyPath: nil, cacheName: "Master")
-        aFetchedResultsController.delegate = self
-        _fetchedResultsController = aFetchedResultsController
-        
-        do {
-            try _fetchedResultsController!.performFetch()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            //print("Unresolved error \(error), \(error.userInfo)")
-            abort()
-        }
-        
-        return _fetchedResultsController!
-    }
-    var _fetchedResultsController: NSFetchedResultsController? = nil
-    */
-    /*
-    func controllerWillChangeContent(controller: NSFetchedResultsController) {
-    self.tableView.beginUpdates()
-    }
-    
-    func controller(controller: NSFetchedResultsController, didChangeSection sectionInfo: NSFetchedResultsSectionInfo, atIndex sectionIndex: Int, forChangeType type: NSFetchedResultsChangeType) {
-    switch type {
-    case .Insert:
-    self.tableView.insertSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Fade)
-    case .Delete:
-    self.tableView.deleteSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Fade)
-    default:
-    return
-    }
-    }
-    
-    func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
-    switch type {
-    case .Insert:
-    tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
-    case .Delete:
-    tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
-    case .Update:
-    self.configureCell(tableView.cellForRowAtIndexPath(indexPath!)!, atIndexPath: indexPath!)
-    case .Move:
-    tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
-    tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
-    }
-    }
-    */
-    
-    func controllerDidChangeContent(controller: NSFetchedResultsController) {
-        //self.tableView.endUpdates()
-    }
-    
-    /*
-    // Implementing the above methods to update the table view in response to individual changes may have performance implications if a large number of changes are made simultaneously. If this proves to be an issue, you can instead just implement controllerDidChangeContent: which notifies the delegate that all section and object changes have been processed.
-    
-    func controllerDidChangeContent(controller: NSFetchedResultsController) {
-    // In the simplest, most efficient, case, reload the table view.
-    self.tableView.reloadData()
-    }
-    */
+
 }
 
